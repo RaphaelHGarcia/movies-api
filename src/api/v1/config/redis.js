91b2a8439185b2fa73bf23824670c018425ac850
RaphaelGarcia <raphagarciaskt@gmail.com>
2017-03-12 18:38:08 -0300

    Deploy heroku redis

--- src/api/v1/config/redis.js
+++ src/api/v1/config/redis.js
@@ -2,7 +2,7 @@
 
 import redis from 'redis';
 
-const client = redis.createClient(42509, 'pc82252fc917fb325225cb2751af99049147b0aa7cfda498f095b7d55f1a5e7ca@ec2-34-198-54-21.compute-1.amazonaws.com');
+const client = redis.createClient(process.env.REDIS_URL);
 const redisService = {
   client,
   time: 7200
