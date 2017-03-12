5c97c798d279d636435241b15029b8b43c20e769
RaphaelGarcia <raphagarciaskt@gmail.com>
2017-03-12 18:28:11 -0300

    Heroku deploy Redis

--- src/api/v1/config/redis.js
+++ src/api/v1/config/redis.js
@@ -2,7 +2,7 @@
 
 import redis from 'redis';
 
-const client = redis.createClient(6379, 'localhost');
+const client = redis.createClient(42509, 'pc82252fc917fb325225cb2751af99049147b0aa7cfda498f095b7d55f1a5e7ca@ec2-34-198-54-21.compute-1.amazonaws.com');
 const redisService = {
   client,
   time: 7200
